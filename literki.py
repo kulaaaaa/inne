f9813dcef69db8b709c1c01518b4cc3baaee370b
matniawarhawks <98478979+matniawarhawks@users.noreply.github.com>
2022-04-11 19:13:04 +0200

    Add files via upload

---
+++ literki.py
@@ -0,0 +1,65 @@
+text = """– Czemu tak ciągle gadasz o kobietach, Stan?
+– Bo chcę zostać kobietą. Chcę być kobietą. Chce żebyście od dziś mówili na mnie „Loretta”. To moje niezbywalne prawo jako mężczyzny.
+– Ale dlaczego chcesz zostać Lorettą, Stan?
+– Bo chcę mieć dzieci.
+– Dzieci?
+– Każdy mężczyzna ma prawo mieć dzieci, jeśli chce.
+– Przecież ty nie możesz mieć dzieci!
+– Nie prześladuj mnie!
+– Nie prześladuję cię, Stan! Nie masz macicy! Gdzie będziesz trzymał swojego embriona? W pudełku?
+– Mam pomysł! Przyjmijmy, że Stan nie może póki co mieć dzieci, gdyż nie ma macicy, co nie jest niczyją winą, nawet Rzymian, ale musimy przyznać, że ma prawo do dzieci!
+– Świetnie, Judith! Będziemy walczyć z ciemiężycielami…
+– Przepraszam.
+– A po co?
+– Co po co?
+– Po co walczyć o jego prawo do posiadania dzieci…
+– To symbol naszej beznadziejnej walki z najeźdźcą.
+– Symbol jego beznadziejnej walki z rzeczywistością."""
+
+
+number_of_a= text.count("a")
+print(number_of_a)
+
+number_of_e= text.count("e")
+print(number_of_e)
+
+number_of_i= text.count("i")
+print(number_of_i)
+
+number_of_o= text.count("o")
+print(number_of_o)
+
+number_of_u= text.count("u")
+print(number_of_u)
+
+number_of_y= text.count("y")
+print(number_of_y)
+
+print("===================")
+
+number_of_A= text.count("A")
+print(number_of_A)
+
+number_of_E= text.count("E")
+print(number_of_E)
+
+number_of_I= text.count("I")
+print(number_of_I)
+
+number_of_O= text.count("O")
+print(number_of_O)
+
+number_of_U= text.count("U")
+print(number_of_U)
+
+number_of_Y= text.count("Y")
+print(number_of_Y)
+
+print("===================")
+print("A + a =", number_of_A + number_of_a)
+print("E + e =", number_of_E + number_of_e)
+print("I + i =", number_of_I + number_of_i)
+print("O + o =", number_of_O + number_of_o)
+print("U + u =", number_of_U + number_of_u)
+print("Y + y =", number_of_Y + number_of_y)
+
